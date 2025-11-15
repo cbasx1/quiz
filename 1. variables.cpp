33a47fcb63b68bd91dd8f2f42052f264e72e93fa
CBAS <sebastian.camilo.buitrago@correounivalle.edu.co>
2025-11-15 11:37:03 -0500

    1. variables.cpp

--- 1. variables.cpp
+++ 1. variables.cpp
@@ -162,3 +162,54 @@ int main() {
 // si no se cumple, pasa a evaluar el else if
 
 // si ninguna condición se cumple, se ejecuta el else final, si existe
+
+// 4 BUCLES
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "=== pregunta 4.1: numeros del 1 al 10 con for ===" << endl;
+
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
+
+    cout << endl; // salta la linea al final
+    return 0;
+}
+
+// 2.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "=== pregunta 4.2: numeros del 10 al 1 con while ===" << endl;
+
+    int i = 10;
+    while (i >= 1) {
+        cout << i << " ";
+        i--; // disminuye en 1
+    }
+
+    cout << endl; // salta la línea al final
+    return 0;
+}
+
+// 3.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 2;
+    cout << "numeros pares entre 1 y 20 usando do-while:" << endl;
+    do {
+        cout << i << " ";
+        i += 2;
+    } while (i <= 20);
+    cout << endl;
+    return 0;
+
+}
