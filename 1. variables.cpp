c3c5030a389e24146d67d9d2b84b24be55c9e51b
CBAS <sebastian.camilo.buitrago@correounivalle.edu.co>
2025-11-15 11:35:49 -0500

    1. variables.cpp

--- 1. variables.cpp
+++ 1. variables.cpp
@@ -79,7 +79,7 @@ int main() {
 // != (diferente de) compara si dos valores son distintos, si lo son, devuelve true (1), si no, devuelve false (0)
 
 
-// 3.
+// 3 CONDICIONALES
 
 #include <iostream>
 using namespace std;
@@ -109,3 +109,56 @@ int main() {
 // || (OR): la condicion es verdadera si al menos una de las expresiones es verdadera
 
 // ! (NOT): invierte el valor logico de la condicion (true a false, false a true)
+
+// 3 CONDICIONALES
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int edad;
+
+    cout << "=== pregunta 3.1: mayor o menor de edad ===" << endl;
+    cout << "ingresa tu edad: ";
+    cin >> edad;
+
+    if (edad >= 18) {
+        cout << "eres mayor de edad." << endl;
+    } else {
+        cout << "eres menor de edad." << endl;
+    }
+
+    return 0;
+}
+
+
+// 2.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nota;
+
+    cout << "=== pregunta 3.2: uso de else if ===" << endl;
+    cout << "ingresa tu nota (0 - 100): ";
+    cin >> nota;
+
+    if (nota >= 90) {
+        cout << "excelente" << endl;
+    } else if (nota >= 70) {
+        cout << "aprobado" << endl;
+    } else if (nota >= 50) {
+        cout << "suficiente" << endl;
+    } else {
+        cout << "reprobado" << endl;
+    }
+
+    return 0;
+}
+
+// en C++, la estructura else if se usa cuando tenemos mas de dos condiciones posibles
+// primero se evalua el if
+// si no se cumple, pasa a evaluar el else if
+
+// si ninguna condición se cumple, se ejecuta el else final, si existe
